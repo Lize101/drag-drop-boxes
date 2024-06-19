fb1c92cdfe19fd6239d9bfb2edfbdad9c785e1c0
Lizdigital <lizetteclaireh@gmail.com>
2024-06-19 14:35:02 +0100

    Add JS drag and drop functionality

---
+++ JS/app.js
@@ -0,0 +1,34 @@
+const left = document.getElementById('left');
+const right = document.getElementById('right');
+const lists = document.getElementsByClassName('list');
+
+
+for (list of lists) {
+    list.addEventListener('dragstart', function(e) {
+        let selected = e.target;
+
+        right.addEventListener('dragover', function(e) {
+            e.preventDefault();
+        })
+
+        right.addEventListener('drop', function(e) {
+            right.appendChild(selected);
+            selected = null;
+        })
+
+        left.addEventListener('dragover', function(e) {
+            e.preventDefault();
+        })
+
+        left.addEventListener('drop', function(e) {
+            left.appendChild(selected);
+            selected = null;
+        })
+    })
+
+
+}
+
+
+
+
